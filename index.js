1d472a35513ee3654fd32c8f856913593f2d645c
Greg Allen <greg@firstandthird.com>
2016-05-23 16:57:21 -0700

    refactored to take a callback

--- index.js
+++ index.js
@@ -3,37 +3,15 @@ var _ = require('lodash');
 exports.register = function(server, options, next) {
   options = options || {};
 
-  var addContext = function(request, key, data) {
-    var response = request.response;
-
-    if (response.variety !== 'view') {
-      return;
-    }
-
-    var d = {};
-
-    if (typeof key === 'string') {
-      d[key] = data;
-    } else {
-      d = key;
-    }
-
-    _.forIn(d, function(value, key) {
-      response.source.context = _.set(response.source.context || {}, key, value);
-    });
-
-    return response.source.context;
-  };
-
-  server.expose('addContext', addContext);
-
-  if (options.context) {
+  server.expose('setViewContext', function(fn) {
     server.ext('onPostHandler', function(request, reply) {
-      addContext(request, options.context);
-
+      var response = request.response;
+      if (response.variety === 'view') {
+        response.source.context = fn(response.source.context || {}, request);
+      }
       reply.continue();
     });
-  }
+  });
 
   if (options.enableDebug) {
     server.on('tail', function(request) {
