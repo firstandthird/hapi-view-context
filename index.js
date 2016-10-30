729fcdbf4447b82f9a9cc8417ad321dbd975f0bb
Chris <orthagonal1@gmail.com>
2016-10-30 15:29:48 -0500

    es6, eslint, tests, functionality

--- index.js
+++ index.js
@@ -3,33 +3,69 @@ const _ = require('lodash');
 
 exports.register = function(server, options, next) {
   options = options || {};
-  const addViewContextHandler = (fn) => {
+  const defaultContextHandler = (request, reply) => {
+    // ignore if this isn't rendering a view:
+    if (request.response.variety !== 'view') {
+      return reply.continue();
+    }
+    const response = request.response;
+    // make sure the view context is initialized:
+    response.source.context = response.source.context ? response.source.context : {};
+    // add the default context specified in options:
+    _.each(options.context, (contextValue, contextKey) => {
+      _.set(response.source.context, contextKey, contextValue);
+    });
+    // add in any additional context that was added to this request with 'addContext':
+    if (request.plugins['hapi-view-context'] && request.plugins['hapi-view-context'].context) {
+      _.each(request.plugins['hapi-view-context'].context, (contextValue, contextKey) => {
+        response.source.context[contextKey] = contextValue;
+      });
+    }
+    // if options specified a default contextHandler, try to process it:
+    let method = options.contextHandler ? options.contextHandler : undefined;
+    if (typeof method === 'string') {
+      method = _.get(server.methods, method);
+    }
+    if (method) {
+      response.source.context = _.defaults(response.source.context, method(response.source.context || {}, request));
+    }
+    // response.source.context should be set up and ready to go now!
+    reply.continue();
+  };
+
+  // register one handler that handles building the context for each request:
+  server.ext('onPostHandler', defaultContextHandler);
+
+  // allow others to add to this request's context:
+  server.expose('addContext', (request, ...context) => {
+    let newContext = {};
+    // context is either an object or a key: value pair
+    if (context.length === 1 && typeof context[0] === 'object') {
+      newContext = _.defaults(context[0], options.context);
+    } else if (context.length === 2) {
+      newContext[context[0]] = context[1];
+    }
+    if (!request.plugins) {
+      request.plugins = {};
+    }
+    if (!request.plugins['hapi-view-context']) {
+      request.plugins['hapi-view-context'] = {
+        context: newContext
+      };
+    } else {
+      request.plugins['hapi-view-context'] = _.defaults(request.plugins['hapi-view-context'].context);
+    }
+  });
+
+  // lets you add additional viewContextHandlers:
+  server.expose('setViewContext', (fn) => {
     server.ext('onPostHandler', (request, reply) => {
       const response = request.response;
       if (response.variety === 'view') {
-        // set up the default context
-        response.source.context = response.source.context ? response.source.context : {};
-        _.each(options.context, (contextValue, contextKey) => {
-          _.set(response.source.context, contextKey, contextValue);
-        });
-        // get any additional context
         response.source.context = fn(response.source.context || {}, request);
       }
       reply.continue();
     });
-  };
-  // if options specified a default contextFunction register it with the server:
-  if (options.contextFunction) {
-    if (typeof options.contextFunction === 'string') {
-      options.contextFunction = _.get(server, options.contextFunction);
-    }
-    if (typeof options.contextFunction === 'function') {
-      addViewContextHandler(options.contextFunction);
-    }
-  }
-  // let others also use the context-setting handler:
-  server.expose('setViewContext', (fn) => {
-    addViewContextHandler(fn);
   });
 
   if (options.enableDebug) {
--- test/test.context.js
+++ test/test.context.js
@@ -7,7 +7,7 @@ const viewContext = require('../');
 
 let server;
 
-lab.experiment('hapi-hooks', () => {
+lab.experiment('hapi-view-context', () => {
   lab.beforeEach((done) => {
     server = new Hapi.Server({
       debug: {
@@ -34,7 +34,7 @@ lab.experiment('hapi-hooks', () => {
     server.stop(done);
   });
 
-  lab.test(' the plugin can be registered with hapi ', { timeout: 10000 }, (done) => {
+  lab.test(' the plugin exposes the addContext and setViewContext methods for hapi ', (done) => {
     server.register({
       register: viewContext,
       options: {
@@ -46,12 +46,13 @@ lab.experiment('hapi-hooks', () => {
       }
     }, (err) => {
       code.expect(err).to.equal(undefined);
+      code.expect(typeof server.plugins['hapi-view-context'].addContext).to.equal('function');
       code.expect(typeof server.plugins['hapi-view-context'].setViewContext).to.equal('function');
       done();
     });
   });
 
-  lab.test('renders a template using the default context ', { timeout: 10000 }, (done) => {
+  lab.test('renders a template including the default context that was passed in options ', (done) => {
     server.register({
       register: viewContext,
       options: {
@@ -70,7 +71,6 @@ lab.experiment('hapi-hooks', () => {
           reply.view('example');
         }
       });
-      server.plugins['hapi-view-context'].setViewContext((context) => context);
       server.inject({
         method: 'POST',
         url: '/',
@@ -83,7 +83,65 @@ lab.experiment('hapi-hooks', () => {
     });
   });
 
-  lab.test('lets you re-set the context ', { timeout: 10000 }, (done) => {
+  lab.test('can add to the context of an individual request', (done) => {
+    let prevValue = 0;
+    server.register({
+      register: viewContext,
+      options: {
+        enableDebug: true,
+        context: {
+          something: 'Something',
+          nested: {
+            something: 'Nested'
+          }
+        }
+      }
+    }, (err) => {
+      let requestCount = 0;
+      code.expect(err).to.equal(undefined);
+      server.route({
+        method: 'POST',
+        path: '/',
+        handler: (request, reply) => {
+          prevValue = Math.random().toString();
+          // subsequent calls to this route will have a new 'request' object, so only the first call to
+          // this view should have prevValue:
+          if (requestCount === 0) {
+            server.plugins['hapi-view-context'].addContext(request, { something: prevValue });
+            requestCount ++;
+          } else {
+            server.plugins['hapi-view-context'].addContext(request, {
+              nested: {
+                something: 'view2'
+              }
+            });
+          }
+          reply.view('example');
+        }
+      });
+      server.inject({
+        method: 'POST',
+        url: '/',
+      }, (response) => {
+        // first call to the route will have prevValue
+        code.expect(response.statusCode).to.equal(200);
+        code.expect(response.result).to.include(prevValue);
+        // second call to the route will not have any of the changes made in the previous call:
+        server.inject({
+          method: 'POST',
+          url: '/',
+        }, (response2) => {
+          code.expect(response2.result).to.not.include(prevValue);
+          code.expect(response2.result).to.include('Something');
+          code.expect(response2.result).to.include('view2');
+          done();
+        });
+      });
+    });
+  });
+
+  lab.test('can let you set a custom handler with setViewContext', (done) => {
+    let prevValue = 0;
     server.register({
       register: viewContext,
       options: {
@@ -101,43 +159,45 @@ lab.experiment('hapi-hooks', () => {
         method: 'POST',
         path: '/',
         handler: (request, reply) => {
+          prevValue = Math.random().toString();
+          server.plugins['hapi-view-context'].addContext(request, { something: prevValue });
           reply.view('example');
         }
       });
-      server.plugins['hapi-view-context'].setViewContext((context) => {
-        context.something = 'view1';
+      let handlerValue = 0;
+      server.plugins['hapi-view-context'].setViewContext((context, request) => {
+        // make sure we got the hapi request object:
+        code.expect(Object.keys(request)).to.include('server');
+        code.expect(Object.keys(request)).to.include('url');
+        code.expect(Object.keys(request)).to.include('method');
+        handlerValue = Math.random().toString();
+        context.something = handlerValue;
         return context;
       });
-
       server.inject({
         method: 'POST',
         url: '/',
       }, (response) => {
         code.expect(response.statusCode).to.equal(200);
-        code.expect(response.result).to.include('view1');
-        server.plugins['hapi-view-context'].setViewContext((context) => {
-          context.nested.something = 'view2';
-          return context;
-        });
+        code.expect(response.result).to.not.include(prevValue);
         server.inject({
           method: 'POST',
           url: '/',
         }, (response2) => {
-          code.expect(response2.result).to.not.include('view1');
-          code.expect(response2.result).to.include('Something');
-          code.expect(response2.result).to.include('view2');
+          code.expect(response2.result).to.not.include(prevValue);
+          code.expect(response2.result).to.include(handlerValue);
           done();
         });
       });
     });
   });
 
-  lab.test('custom context method', (done) => {
+  lab.test('can let you pass a default custom handler in the options', (done) => {
     server.register({
       register: viewContext,
       options: {
         enableDebug: true,
-        contextFunction: (context) => {
+        contextHandler: (context) => {
           context.methodValue = 'method';
           return context;
         },
@@ -153,6 +213,42 @@ lab.experiment('hapi-hooks', () => {
         handler: (request, reply) => {
           reply.view('method');
         }
+      });
+      server.inject({
+        method: 'POST',
+        url: '/',
+      }, (response) => {
+        code.expect(response.statusCode).to.equal(200);
+        code.expect(response.result).to.include('default');
+        code.expect(response.result).to.include('method');
+        done();
+      });
+    });
+  });
+
+  lab.test('can let you use a named server method (even one that may not have been registered with the server yet) as the default custom handler', (done) => {
+    server.register({
+      register: viewContext,
+      options: {
+        enableDebug: true,
+        contextHandler: 'serverMethod',
+        context: {
+          defaultValue: 'default'
+        }
+      }
+    }, (err) => {
+      code.expect(err).to.equal(undefined);
+      server.route({
+        method: 'POST',
+        path: '/',
+        handler: (request, reply) => {
+          reply.view('method');
+        }
+      });
+      // the server method we will use as the contextHandler,
+      server.method('serverMethod', (context) => {
+        context.methodValue = 'method';
+        return context;
       });
       server.inject({
         method: 'POST',
